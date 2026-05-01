f7add45e1f5bdad758e82fe9aff6c2517dda77ea
fall-mansour <mansourbabydriver221@gmail.com>
2026-05-01 06:46:20 +0000

    Update API URL for production environment

--- Terranga_motors/src/environnement.ts
+++ Terranga_motors/src/environnement.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true, // Passe à true si tu es en train de déployer
-  apiUrl: 'https://teranga-motors-backend.onrender.com/api',
+  apiUrl: 'https://teranga-backend-jui8.onrender.com/api',
 };
