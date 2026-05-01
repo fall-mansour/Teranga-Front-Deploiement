e0f27b7e83950d26bb1055974a20417c674e8533
fall-mansour <mansourbabydriver221@gmail.com>
2026-05-01 10:15:21 +0000

    Fix import path for environment in pieces copie.ts

--- Terranga_motors/src/app/pieces copie.ts
+++ Terranga_motors/src/app/pieces copie.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../environnement';
+import { environment } from './environnement';
 
 export interface Piece {
   id?: number;
