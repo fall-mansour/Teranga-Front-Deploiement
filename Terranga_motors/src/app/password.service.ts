036a9a0e8a9f1c96a28a08f0675906e674745fab
fall-mansour <mansourbabydriver221@gmail.com>
2026-05-01 10:14:48 +0000

    Fix import path for environment in password.service.ts

--- Terranga_motors/src/app/password.service.ts
+++ Terranga_motors/src/app/password.service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../environnement'; // ✅ Chemin à vérifier selon ton arborescence
+import { environment } from './environnement'; // ✅ Chemin à vérifier selon ton arborescence
 
 @Injectable({ providedIn: 'root' })
 export class PasswordService {
